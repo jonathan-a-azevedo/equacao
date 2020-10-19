6e34456e51745757eb1f9b25e6ef35ad45c938fa
jonathan-a-azevedo <70524114+jonathan-a-azevedo@users.noreply.github.com>
2020-10-19 15:58:13 -0300

    Update atvs.js

--- atvs.js
+++ atvs.js
@@ -3,5 +3,5 @@ let a, b, c, delta;
 a = prompt('Digite o valor de "a":');
 b = prompt('Digite o valor de "b":');
 c = prompt('Digite o valor de "c":');
-delta = (b*b)-4*a*c;
+delta = (b*b)-4*a*c; 
 alert(`x¹= ${[-b-Math.sqrt(delta)]/(2*a)} e x²= ${[-b+Math.sqrt(delta)]/(2*a)}`);
